d07297bde2ed1aceb8a8c984efc8c8044ba98d54
CHEN LIYANG <cly753@gmail.com>
2015-05-05 10:23:48 +0800

    wu...

--- src/main/java/oj/judge/center/Center.java
+++ src/main/java/oj/judge/center/Center.java
@@ -4,7 +4,9 @@ import java.nio.file.Path;
 
 import oj.judge.common.Callback;
 import oj.judge.common.Conf;
+import oj.judge.common.Problem;
 import oj.judge.common.Solution;
+import oj.judge.remote.Remote;
 import oj.judge.runner.Runner;
 
 public class Center extends Thread {
@@ -20,33 +22,79 @@ public class Center extends Thread {
     	if (!Conf.init())
     		return ;
 
-//        Remote remote = new Remote(Conf.fetchInterval());
-//        try {
-//            byte[] raw = remote.getProblemResourcesZip(1L);
-//
-//            Problem problem = new Problem(1L, "", raw);
-//            System.out.println(problem);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        int aaa = 1;
+        switch (aaa) {
+            case 1:
+                // try fetch problem
+                tryFetchProblem();
+                break;
+            case 2:
+                // try judge a mock solution
+                tryJudgeMockSolution();
+                break;
+            case 3:
+                // all together
+                tryAllTogether();
+                break;
+        }
+	}
 
-//        System.out.println(solution);
+    public void tryFetchProblem() {
+        Remote remote = new Remote(Conf.fetchInterval());
+        try {
+            byte[] raw = remote.getProblemResourcesZip(1L);
 
+            Problem problem = new Problem(1L, "", raw);
+            System.out.println(problem);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tryJudgeMockSolution() {
         Integer id = 0;
-    	Path savePath = Conf.runningPath();
+        Path savePath = Conf.runningPath();
         Runner r = new Runner(id, savePath, Solution.getMockSolution());
 
         r.setName("Runner-" + id);
         r.reg(Runner.E.FINISH, new Callback() {
             @Override
             public void call() {
-            	if (Conf.debug()) System.out.println(label + "Callback Runner.E.FINISH");
-            	if (Conf.debug()) System.out.println(label + o);
-                if (Conf.debug()) System.out.println(label + ((Solution)o).getResultJson());
+                if (Conf.debug()) System.out.println(label + "Callback Runner.E.FINISH");
+                if (Conf.debug()) System.out.println(label + o);
+                if (Conf.debug()) System.out.println(label + ((Solution) o).getResultJson());
             }
         });
 
         r.start();
         if (Conf.debug()) System.out.println("Waiting for result...");
-	}
+    }
+
+    public void tryAllTogether() {
+        final Remote remote = new Remote(Conf.fetchInterval());
+        remote.reg(Remote.E.NEWPROBLEM, new Callback() {
+            @Override
+            public void call() {
+                Integer id = 0;
+                Path savePath = Conf.runningPath();
+                Runner r = new Runner(id, savePath, (Solution)o);
+
+                r.setName("Runner-" + id);
+                r.reg(Runner.E.FINISH, new Callback() {
+                    @Override
+                    public void call() {
+                        if (Conf.debug()) System.out.println(label + "Callback Runner.E.FINISH");
+                        if (Conf.debug()) System.out.println(label + o);
+                        if (Conf.debug()) System.out.println(label + ((Solution) o).getResultJson());
+
+                        remote.pushSolution((Solution)o);
+                    }
+                });
+
+                r.start();
+                if (Conf.debug()) System.out.println("Waiting for result...");
+            }
+        });
+        remote.start();
+    }
 }
